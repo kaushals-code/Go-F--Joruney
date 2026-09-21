64f07d2a45423b06cceb711da4e2594c124b32a5
kaushals-code <kaushal21gs@gmail.com>
2026-09-21 11:23:53 +0530

    Day 24 extra commit

---
+++ fsharphttp/learning/Program.fs
@@ -0,0 +1,23 @@
+﻿open System.Text.Json
+
+type Todo = {
+    Id: int
+    Title: string
+    Completed: bool
+}
+
+let todo = {
+    Id= 1
+    Title= "Listen to The Weeknd"
+    Completed= true
+}
+
+let option = JsonSerializerOptions(
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+)
+let res = JsonSerializer.Serialize(todo, option)
+
+let actual = JsonSerializer.Deserialize<Todo>(res)
+
+printfn "%s" res
+printfn "%A" actual
